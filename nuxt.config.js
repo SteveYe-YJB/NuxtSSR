4a186a905b27e796fc81d71c2f5e6c1fea6e86a7
SteveYe-YJB <790021919@qq.com>
2021-12-16 21:58:06 +0800

    修改访问权限

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ module.exports = {
   router: {
     linkActiveClass: 'active',
     // 自定义路由表规则
-    extendRoutes (routes, resolve) {
+    extendRoutes(routes, resolve) {
       // 清除 Nuxt.js 基于 pages 目录默认生成的路由表规则
       routes.splice(0)
 
@@ -57,7 +57,7 @@ module.exports = {
   },
 
   server: {
-    host: '0.0.0.0',
+    host: 'localhost',
     port: 3000
   },
 
